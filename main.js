1a4d3bc4e54dbefb991dd3137bbddd26e3455daa
FuhuaChen <874166712@qq.com>
2017-11-24 17:08:25 +0800

    添加清除、保存

--- main.js
+++ main.js
@@ -20,6 +20,21 @@ brush.onclick = function () {
     eraser.classList.remove('active')
 }
 
+//清除
+clear.onclick = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+//保存
+download.onclick = function () {
+    var url = canvas.toDataURL('image/png')
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = '我的图画'
+    a.click()
+}
+
 //画笔颜色
 black.onclick = function () {
     context.fillStyle = 'black'
@@ -75,9 +90,9 @@ var drawLine = function (x1, y1, x2, y2) {
     context.stroke()
     context.closePath()
 }
-var drawPoint = function (x,y,radius) {
+var drawPoint = function (x, y, radius) {
     context.beginPath()
-    context.arc(x, y, radius, 0, Math.PI*2)
+    context.arc(x, y, radius, 0, Math.PI * 2)
     context.fill()
 }
 
@@ -86,6 +101,7 @@ function autoSetCancasSize(canvas) {
     window.onresize = function () {
         setCanvasSize()
     }
+
     function setCanvasSize() {
         var pageWidth = document.documentElement.clientWidth
         var pageHeight = document.documentElement.clientHeight
@@ -95,12 +111,12 @@ function autoSetCancasSize(canvas) {
 }
 
 function listenUser(canvas) {
-    if(document.body.ontouchstart!==undefined){
+    if (document.body.ontouchstart !== undefined) {
         canvas.ontouchstart = function (xxx) {
             var x = xxx.touches[0].clientX
             var y = xxx.touches[0].clientY
             using = true
-            drawPoint(x,y,lineWidth/2)
+            drawPoint(x, y, lineWidth / 2)
             if (eraserEnabled) {
                 context.clearRect(x - 10, y - 10, 20, 20)
             } else {
@@ -123,7 +139,7 @@ function listenUser(canvas) {
         canvas.ontouchend = function () {
             using = false
         }
-    }else{
+    } else {
         var using = false
         var lastPoint = {x: undefined, y: undefined}
 
@@ -131,7 +147,7 @@ function listenUser(canvas) {
             var x = xxx.clientX
             var y = xxx.clientY
             using = true
-            drawPoint(x,y,lineWidth/2)
+            drawPoint(x, y, lineWidth / 2)
             if (eraserEnabled) {
                 context.clearRect(x - 10, y - 10, 20, 20)
             } else {
